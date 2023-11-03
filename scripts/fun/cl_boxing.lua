932a82ba199ddd6b194fd339487893935c27fa6e
derpy54320 <daboss7000@gmail.com>
2023-11-03 01:03:50 -0400

    Update cl_boxing.lua

--- scripts/fun/cl_boxing.lua
+++ scripts/fun/cl_boxing.lua
@@ -1,26 +1,14 @@
 function main()
 	while true do
-		if PedMePlaying(gPlayer,"Default_KEY",true) then
-			if PlayerIsInAreaXYZ(-711.54,372.19,293.91,1) then
-				if IsButtonBeingPressed(8,0) then
-					PlayerSetPosSimple(PlayerGetPosXYZ(),374,295)
-				else
-					SetTextFont("Comic Sans MS")
-					SetTextBold()
-					SetTextPosition(0.5,0.95)
-					SetTextColor(255,255,255,255)
-					DrawText("Press JUMP to get in the ring.")
-				end
-			elseif PlayerIsInAreaXYZ(-711.54,374,295,1) then
-				if IsButtonBeingPressed(8,0) then
-					PlayerSetPosSimple(PlayerGetPosXYZ(),372.19,293.91)
-				else
-					SetTextFont("Comic Sans MS")
-					SetTextBold()
-					SetTextPosition(0.5,0.95)
-					SetTextColor(255,255,255,255)
-					DrawText("Press JUMP to get out the ring.")
-				end
+		if PlayerIsInAreaXYZ(-711.54,372.19,293.91,1) and PedMePlaying(gPlayer,"Default_KEY",true) then
+			if IsButtonBeingPressed(8,0) then
+				PlayerSetPosSimple(PlayerGetPosXYZ(),374,295)
+			else
+				SetTextFont("Comic Sans MS")
+				SetTextBold()
+				SetTextPosition(0.5,0.95)
+				SetTextColor(255,255,255,255)
+				DrawText("Press JUMP to get in the ring.")
 			end
 		end
 		Wait(0)
