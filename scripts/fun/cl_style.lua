24a760a6c5884c9a4c153f0421bc84f4a80adfd3
derpy54320 <daboss7000@gmail.com>
2023-11-03 00:28:33 -0400

    Update cl_style.lua

--- scripts/fun/cl_style.lua
+++ scripts/fun/cl_style.lua
@@ -12,8 +12,6 @@ function main()
 		end
 		if player and PedGetWeapon(gPlayer) == -1 and PedMePlaying(gPlayer,"DEFAULT_KEY",true) and not PedIsPlaying(gPlayer,"/G/PLAYER",true) then
 			PedSetActionTree(gPlayer,"/Global/Player","Act/Player.act")
-		elseif IsButtonBeingPressed(8,0) and PedIsPlaying(gPlayer,"/G/PLAYER/JUMPACTIONS/JUMP",true) and (PedMePlaying(gPlayer,"RUNJUMP",true) or PedMePlaying(gPlayer,"SPRINTJUMP",true)) then
-			PlayerSetPosSimple(PedGetOffsetInWorldCoords(gPlayer,0,1,0))
 		end
 		Wait(0)
 	end
