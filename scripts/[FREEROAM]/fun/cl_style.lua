2de8fa0f16262076b730cf45ba97e65dedef6945
derpy54320 <daboss7000@gmail.com>
2023-11-06 19:40:49 -0500

    use ped walk but player fighting

--- scripts/[FREEROAM]/fun/cl_style.lua
+++ scripts/[FREEROAM]/fun/cl_style.lua
@@ -1,17 +1,17 @@
 function main()
-	local player = false
-	if PedGetAmmoCount(gPlayer,437) < 1 then
-		GiveWeaponToPlayer(437,1)
-	end
 	while true do
-		if (IsKeyBeingPressed("G",0) or IsKeyBeingPressed("G",1)) and PedMePlaying(gPlayer,"DEFAULT_KEY",true) then
-			if player then
+		if PedMePlaying(gPlayer,"OFFENSE",true) then
+			PedSetActionNode(gPlayer,"/G","")
+		elseif PedMePlaying(gPlayer,"DEFAULT_KEY",true) then
+			if PedIsPlaying(gPlayer,"/G/PLAYER/DEFAULT_KEY",true) then
 				PedSetActionTree(gPlayer,"","")
+			elseif IsButtonBeingPressed(6,0) then
+				PedSetActionNode(gPlayer,"/G/PLAYER/ATTACKS/STRIKES/LIGHTATTACKS/LEFT1","")
+			elseif IsButtonBeingPressed(7,0) then
+				PedSetActionNode(gPlayer,"/G","")
+			elseif IsButtonBeingPressed(15,0) then
+				PedSetFlag(gPlayer,2,not PedGetFlag(gPlayer,2))
 			end
-			player = not player
-		end
-		if player and PedGetWeapon(gPlayer) == -1 and PedMePlaying(gPlayer,"DEFAULT_KEY",true) and not PedIsPlaying(gPlayer,"/G/PLAYER",true) then
-			PedSetActionTree(gPlayer,"/Global/Player","Act/Player.act")
 		end
 		Wait(0)
 	end
