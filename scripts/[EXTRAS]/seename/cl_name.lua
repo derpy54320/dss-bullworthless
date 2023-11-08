ce07ac4ad71deaabc6e686b66f2cdaf00121e386
derpy54320 <daboss7000@gmail.com>
2023-11-08 16:18:15 -0500

    Update cl_name.lua

--- scripts/[EXTRAS]/seename/cl_name.lua
+++ scripts/[EXTRAS]/seename/cl_name.lua
@@ -7,15 +7,30 @@ function main()
 		if PedIsValid(target) and net.basync then
 			local ped = net.basync.get_ped_by_ped(target)
 			if ped then
+				local name = get_name(ped)
 				SetTextFont("Arial")
 				SetTextBlack()
 				SetTextColor(230,230,50,255)
 				SetTextOutline()
 				SetTextAlign("C","C")
 				SetTextPosition(0.5,0.95)
-				DrawText("["..ped:get_name().."]")
+				DrawText("["..name.."]")
 			end
 		end
 		Wait(0)
 	end
+end
+function get_name(ped)
+	if not ped:is_player() then
+		local real = ped:get_ped()
+		if PedIsValid(real) then
+			local name = PedGetName(real)
+			local localized = GetLocalizedText(name)
+			if localized then
+				return localized
+			end
+			return string.sub(localized,3)
+		end
+	end
+	return ped:get_name()
 end
