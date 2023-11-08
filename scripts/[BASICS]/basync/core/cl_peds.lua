6707e94ea4a66beac82f35d54e7121dcac88b77b
derpy54320 <daboss7000@gmail.com>
2023-11-08 16:07:58 -0500

    Update cl_peds.lua

--- scripts/[BASICS]/basync/core/cl_peds.lua
+++ scripts/[BASICS]/basync/core/cl_peds.lua
@@ -403,7 +403,7 @@ function validate_peds() -- create / delete peds
 			local real = PedCreateXYZ(ped.server.model,x,y,z) -- create the closest peds that there is space for
 			if PedIsValid(real) then
 				if ped.server.type == "player" then
-					AddBlipForChar(real,1,0,4)
+					AddBlipForChar(real,0,1,1)
 				end
 				PedSetAlpha(real,0,false) -- make the ped fade in on spawn
 				set_ped(ped,real)
