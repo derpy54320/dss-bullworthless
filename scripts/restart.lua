c7a159c40ed9a3dbec83e6c1feadf7ac2dbb7de2
derpy54320 <daboss7000@gmail.com>
2023-11-09 19:11:34 -0500

    Update restart.lua

--- scripts/restart.lua
+++ scripts/restart.lua
@@ -1,18 +1,34 @@
 -- the linux server is configured to automatically restart the server when it exits
 
+gRestarting = false
+
 function main()
 	local ms = 6 * 60 * 60000
 	Wait(ms - 5 * 60000)
-	for i = 5,1,-1 do
-		notify("Server restarting in "..i.." seconds.")
+	gRestarting = true
+	for i = 5,2,-1 do
+		notify("Server restarting in "..i.." minutes.")
 		Wait(60000)
 	end
+	notify("Server restarting in 1 minute.")
+	Wait(60000)
 	notify("Server restarting!")
 	Wait(1000)
 	QuitServer()
+	TerminateCurrentScript()
 end
 function notify(text)
 	if net.chat then
 		net.chat.notify(-1,text)
 	end
 end
+RegisterLocalEventHandler("PlayerListing",function(player,listing)
+	if gRestarting then
+		listing.info = "The server is restarting, check back in a few minutes!"
+	end
+end)
+RegisterLocalEventHandler("PlayerConnecting",function(player)
+	if gRestarting then
+		KickPlayer(player,"The server is restarting, try again in a few minutes!")
+	end
+end)
