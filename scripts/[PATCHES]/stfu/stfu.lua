41d93558609e3a41ed816ca2c8458929548acf59
derpy54320 <derpy54320@gmail.com>
2024-05-03 23:17:58 -0400

    Update stfu.lua

--- scripts/[PATCHES]/stfu/stfu.lua
+++ scripts/[PATCHES]/stfu/stfu.lua
@@ -1,5 +1,5 @@
 function main()
-	while net_ped in net.basync.all_peds() do
+	for net_ped in net.basync.all_peds() do
 		local ped = net_ped:get_ped()
 		if net_ped:is_player() and PedIsValid(ped) and SoundSpeechPlaying(ped) then
 			SoundStopAmbientSpeechEvent(ped)
