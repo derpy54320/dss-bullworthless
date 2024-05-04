6dd14dcbf199c40a972e4921b24dbfb38c44bdf8
derpy54320 <derpy54320@gmail.com>
2024-05-03 23:20:11 -0400

    Update stfu.lua

--- scripts/[PATCHES]/stfu/stfu.lua
+++ scripts/[PATCHES]/stfu/stfu.lua
@@ -1,9 +1,10 @@
 function main()
-	for net_ped in net.basync.all_peds() do
-		local ped = net_ped:get_ped()
-		if net_ped:is_player() and PedIsValid(ped) and SoundSpeechPlaying(ped) then
-			print("shut the fuck up "..net_ped:get_name())
-			SoundStopCurrentSpeechEvent(ped)
+	while true do
+		for net_ped in net.basync.all_peds() do
+			local ped = net_ped:get_ped()
+			if net_ped:is_player() and PedIsValid(ped) and SoundSpeechPlaying(ped) then
+				SoundStopCurrentSpeechEvent(ped)
+			end
 		end
 		Wait(0)
 	end
